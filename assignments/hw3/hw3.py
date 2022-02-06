cd531bf758b648e721c31f7c515a4ba4d738907a
davisja2022 <davisja@g.cofc.edu>
2022-02-05 22:52:21 -0500

    hw 3 push

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -1,5 +1,5 @@
 """
-Name: <your name goes here – first and last>
+Name: <Jessica Davis>
 <ProgramName>.py
 
 Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
@@ -12,23 +12,52 @@ I certify that this assignment is my own work, but I discussed it with: <Name(s)
 
 
 def average():
-    pass
+    grades = int(input('how many grades will you enter?:'))
+    grades_acc = 0.0
+    for i in range(grades):
+        print('enter grade:')
+        grades_entered = eval(input(' '))
+        grades_acc = grades_acc + grades_entered
+    print('average is: ', grades_acc / grades)
 
 
 def tip_jar():
-    pass
+    tip_acc = 0.0
+    for i in range(5):
+        print('how much would you like to donate?:')
+        donation_input = eval(input(' '))
+        tip_acc = tip_acc + donation_input
+    print('total tips:', tip_acc)
 
 
 def newton():
-    pass
+    x_input = int(input('what number do you want to square root?: '))
+    approximation_input = int(input('how many times would you like to improve the approximation?:'))
+    for i in range(approximation_input):
+        square_root = approximation_input + (x_input/approximation_input)
+        final_approx = square_root / 2
+    print('the approximation of the square root is', final_approx)
+
 
 
 def sequence():
-    pass
+    terms = eval(input('how many terms would you like?:'))
+    for i in range(1, terms + 1):
+
+
+
+
+
 
 
 def pi():
-    pass
+    pi_terms = eval(input('how many terms in the series?:'))
+    pi_acc = 0.0
+
+    print(pi_result)
+
+
+
 
 
 if __name__ == '__main__':
